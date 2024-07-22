91c4cd0be2b1a0aefbed0afe9ca1ca6ac0d471d6
Sakariyah Abdulhazeem <sakariyahabdulhazeem@gmail.com>
2024-07-22 04:29:40 +0100

    autocomplte-disable

--- app/profile/signup/page.tsx
+++ app/profile/signup/page.tsx
@@ -97,6 +97,7 @@ const Signup = () => {
                 <input
                   type="password"
                   id="password"
+                  autoComplete="none"
                   value={password}
                   onChange={(e) => setPassword(e.target.value)}
                   className="mt-1 block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-red-500 focus:border-red-500"
