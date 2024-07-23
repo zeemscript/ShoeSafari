99780815201ce8b9e45dcdea3d530a72559f77c5
Sakariyah Abdulhazeem <sakariyahabdulhazeem@gmail.com>
2024-07-23 15:58:18 +0100

    upadte

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,48 +1,70 @@
-"use client"
-import "/styles/global.css";
+import "../styles/global.css";
 import Navbar from "../components/Navbar";
 import ScrollToTop from "../components/ScrollToTop";
-import Whatsapp from "../components/Whatsapp"
+import Whatsapp from "../components/Whatsapp";
 import Footer from "../components/Footer";
-import { AuthProvider } from '../lib/AuthContext';
+import { AuthProvider } from "../lib/AuthContext";
+import Head from "next/head";
 
+export const metadata = {
+  title: "SHOESAFARI",
+  description: "YOUR OUTFIT",
+  openGraph: {
+    type: "website",
+    url: "https://shoesafari.vercel.app/",
+    title: "SHOESAFARI",
+    description: "YOUR OUTFIT",
+    images: [
+      {
+        url: "https://shoesafari.vercel.app/images/og-image.png",
+        width: 1200,
+        height: 630,
+        alt: "SHOESAFARI",
+      },
+    ],
+  },
+};
 
-// export const metadata = {
-//   title: "SHOESAFARI",
-//   description: "YOUR OUTFIT",
-//   openGraph: {
-//     type: "website",
-//     url: "https://shoesafari.vercel.app/",
-//     title: "SHOESAFARI",
-//     description: "YOUR OUTFIT",
-//     images: [
-//       {
-//         url: "https://shoesafari.vercel.app/images/og-image.png",
-//         width: 1200,
-//         height: 630,
-//         alt: "SHOESAFARI",
-//       },
-//     ],
-//   },
-// };
-  
 const RootLayout = ({ children }) => {
   return (
-    <AuthProvider>
-      <html lang="en">
-        <link rel="icon" href="/images/shoelogoo.png" />
-        <meta property="og:image" content="/images/shoelogoo.png"/>
-        <body className="h-full ">
+    <html lang="en">
+      <Head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <meta property="og:type" content={metadata.openGraph.type} />
+        <meta property="og:url" content={metadata.openGraph.url} />
+        <meta property="og:title" content={metadata.openGraph.title} />
+        <meta
+          property="og:description"
+          content={metadata.openGraph.description}
+        />
+        <meta property="og:image" content={metadata.openGraph.images[0].url} />
+        <meta
+          property="og:image:width"
+          content={metadata.openGraph.images[0].width}
+        />
+        <meta
+          property="og:image:height"
+          content={metadata.openGraph.images[0].height}
+        />
+        <meta
+          property="og:image:alt"
+          content={metadata.openGraph.images[0].alt}
+        />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
+      <body className="h-full">
+        <AuthProvider>
           <div className="flex flex-col min-h-screen">
             <Navbar />
             <main className="app flex-grow">{children}</main>
-            <Whatsapp/>
+            <Whatsapp />
             <ScrollToTop />
             <Footer />
           </div>
-        </body>
-      </html>
-    </AuthProvider>
+        </AuthProvider>
+      </body>
+    </html>
   );
 };
 
